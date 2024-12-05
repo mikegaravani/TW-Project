64b7c104b13960b748e271c3fbb33bfb558d8498
Mike Garavani <scotchmagico@gmail.com>
2024-12-05 23:01:28 +0100

    add pomo SP builder page

---
+++ client/src/components/pomodoro-components/SPBForm.jsx
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import Alert from "../reusables/Alert";
+
+function SPBForm() {
+  const [hours, setHours] = useState("2");
+  const [minutes, setMinutes] = useState("30");
+
+  const [cyclesType, setCyclesType] = useState("long");
+
+  const handleHoursChange = (e) => {
+    let value = parseInt(e.target.value, 10);
+    if (isNaN(value)) value = 0;
+    if (value >= 0 && value <= 23) {
+      setHours(value.toString());
+    }
+  };
+
+  const handleMinutesChange = (e) => {
+    let value = parseInt(e.target.value, 10);
+    if (isNaN(value)) value = 0; // Default to 0 if the input is empty or invalid
+    if (value >= 0 && value <= 59) {
+      setMinutes(value.toString());
+    }
+  };
+
+  const handleCyclesType = (type) => {
+    setCyclesType(type);
+  };
+
+  return (
+    <>
+      <div className="flex-1 lg:text-lg">
+        {/* TITLE STYLES */}
+        {/* <div className="text-left text-xl font-semibold text-gray-800 mb-4"></div> */}
+
+        {/* <Alert /> */}
+
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-1">
+            <label className="text-left text-gray-700 font-bold mb-1">
+              Choose your total session length:
+            </label>
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                value={hours}
+                onChange={handleHoursChange}
+                className="w-16 border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="2"
+                min="0"
+                max="23"
+              />
+              <span>{hours === "1" ? "hour" : "hours"} and</span>
+              <input
+                type="number"
+                value={minutes}
+                onChange={handleMinutesChange}
+                className="w-16 border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="30"
+                min="0"
+                max="59"
+              />
+              <span>{minutes === "1" ? "minute" : "minutes"}</span>
+            </div>
+
+            <label className="text-left text-gray-700 font-bold mb-1 mt-6">
+              Choose your session intensity:
+            </label>
+            <div className="flex justify-evenly gap-3 lg:h-16 text-sm lg:text-xl">
+              <button
+                type="button"
+                class="flex-1 text-white bg-blue-500 hover:bg-blue-400 focus:bg-black font-medium rounded-lg px-5 py-2.5 me-2 mb-2 focus:outline-none hover:text-white"
+              >
+                CHILL
+              </button>
+              <button
+                type="button"
+                class="flex-1 text-white bg-blue-500 hover:bg-blue-400 focus:bg-black font-medium rounded-lg px-5 py-2.5 me-2 mb-2 focus:outline-none hover:text-white"
+              >
+                MEDIUM
+              </button>
+              <button
+                type="button"
+                class="flex-1 text-white bg-blue-500 hover:bg-blue-400 focus:bg-black font-medium rounded-lg px-5 py-2.5 me-2 mb-2 focus:outline-none hover:text-white"
+              >
+                INTENSE
+              </button>
+            </div>
+
+            <div className="bg-gray-200 rounded-lg mt-1 px-4 py-3">
+              <p className="font-bold">About the session intensities:</p>
+              <p className="text-sm">
+                You can choose the ratio of relax time to focus time. By
+                default, the <span className="font-semibold">CHILL</span>{" "}
+                session has 20% relax, the{" "}
+                <span className="font-semibold">MEDIUM</span> has 15%, and if
+                you're really feeling it, the{" "}
+                <span className="font-semibold">INTENSE</span> has only 10%
+                relax time.
+              </p>
+            </div>
+
+            <label className="text-left text-gray-700 font-bold mb-1 mt-6">
+              Choose your cycle length:
+            </label>
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center">
+                <input
+                  type="checkbox"
+                  id="longer-focus"
+                  checked={cyclesType === "long"}
+                  onChange={() => handleCyclesType("long")}
+                  className="mr-2"
+                />
+                <label htmlFor="longer-focus" className="text-gray-700">
+                  Longer focus times, longer relax times
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  type="checkbox"
+                  id="shorter-focus"
+                  checked={cyclesType === "short"}
+                  onChange={() => handleCyclesType("short")}
+                  className="mr-2"
+                />
+                <label htmlFor="shorter-focus" className="text-gray-700">
+                  Shorter focus times, shorter relax times
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SPBForm;
--- client/src/components/pomodoro-components/SPBuilder.jsx
+++ client/src/components/pomodoro-components/SPBuilder.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 import arrowBack from "../../assets/arrow-back.png";
+import SPBForm from "./SPBForm";
 
 function SPBuilder() {
   return (
     <>
       <div className="bg-gray-100 min-h-screen flex flex-col items-center pb-4">
-        <button className="flex items-center gap-2 lg:!gap-4 pt-4 lg:pt-10 pl-4 lg:pl-10 bg-purple-700 self-start">
+        <button className="flex items-center gap-2 lg:gap-4 pt-4 lg:pt-8 pl-4 lg:pl-8 self-start">
           <img
             src={arrowBack}
             alt="Back Arrow"
-            className="h-5 w-5 lg:h-8 lg:w-8"
+            className="h-5 w-5 lg:h-7 lg:w-7"
           />
-          <span className="text-gray-800 font-semibold text-base lg:text-4xl">
+          <span className="text-gray-800 font-semibold text-base lg:text-3xl">
             Back
           </span>
         </button>
         <header className="text-start inline-block text-3xl text-gray-800 my-5 font-bold font-sans shadow-sm transition-transform duration-300 whitespace-nowrap">
-          <h1 className="text-3xl font-bold">BUILD YOUR SESSION</h1>
+          <h1 className="text-3xl lg:text-5xl font-bold">BUILD YOUR SESSION</h1>
         </header>
+
+        <main className="flex flex-col justify-center items-stretch gap-8 mt-8 px-4 lg:px-16 w-full max-w-3xl">
+          <div className="bg-white shadow-lg rounded-lg p-6 flex-1">
+            <SPBForm />
+          </div>
+          <button className="bg-blue-500 text-white font-semibold py-2 rounded-lg hover:bg-blue-300 hover:text-white">
+            Start
+          </button>
+        </main>
       </div>
     </>
   );
